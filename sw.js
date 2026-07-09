17b1b9e52751294036f9f1039bd459ba4384a616
chandanjha2016-prog <chandanjha2016@gmail.com>
2026-07-09 12:45:27 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,9 @@
 
+self.addEventListener('install', e => {
+  console.log("AI Gurukul Learn & Earn App Install ho gaya");
+});
+
+self.addEventListener('fetch', e => {
+  // Offline support basic
+  console.log("App chal raha hai");
+});
